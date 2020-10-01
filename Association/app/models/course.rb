937e6b378ac5bb25840a3299a85365a1e86f4952
Chris <christopher.lopez.09@gmail.com>
2020-10-01 15:26:16 -0400

    complete project

---
+++ Association/app/models/course.rb
@@ -0,0 +1,22 @@
+class Course < ApplicationRecord
+    belongs_to :prerequisite,
+    foreign_key: :prereq_id,
+    primary_key: :id,
+    class_name: :Course,
+    optional: true
+
+    belongs_to :instructor,
+    foreign_key: :instructor_id,
+    primary_key: :id,
+    class_name: :Course
+ 
+
+    has_many :enrollments,
+    foreign_key: :course_id,
+    primary_key: :id,
+    class_name: :Enrollment
+
+    has_many :enrolled_courses,
+    through: :enrollments,
+    source: :user 
+end
